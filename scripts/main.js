41f6efded14cebed9b25b3ac940b70a13a64fa5d
Evgeniy <Masquered@yandex.ru>
2025-03-17 13:48:25 +0500

    fix burger-menu

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,15 @@ import Hero from "./Hero.js";
 
 new Header();
 Hero();
+
+// Закрытие бурегер-меню при клике на ссылку
+var menuLinks = document.querySelectorAll(".header__menu .header__menu-link");
+menuLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    if (
+      document.querySelector(".header__menu").classList.contains("is-active")
+    ) {
+      document.querySelector(".burger-button").click();
+    }
+  });
+});
