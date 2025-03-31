090907b9afb2049a007a8e6fce984c503f97bc9d
Evgeniy Redko <evgeniy.redko@yandex.ru>
2025-03-31 20:57:09 +0500

    add preloader

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,14 @@
 import Header from "./Header.js";
 import Hero from "./Hero.js";
 
+window.onload = function () {
+  document.body.classList.add("loaded_hiding");
+  window.setTimeout(function () {
+    document.body.classList.add("loaded");
+    document.body.classList.remove("loaded_hiding");
+  }, 500);
+};
+
 var menuLinks = document.querySelectorAll(".header__menu .header__menu-link");
 menuLinks.forEach(function (link) {
   link.addEventListener("click", function () {
